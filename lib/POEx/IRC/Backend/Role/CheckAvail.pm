6490b14e2e7d433e731590a2bbc8c878e21c9494
Jon Portnoy <avenj@cobaltirc.org>
2016-07-25 17:12:16 -0400

    safer CheckAvail require()s

--- lib/POEx/IRC/Backend/Role/CheckAvail.pm
+++ lib/POEx/IRC/Backend/Role/CheckAvail.pm
@@ -10,6 +10,8 @@ my %_can_haz;
 
 sub has_ssl_support {
   unless (defined $_can_haz{ssl}) {
+    local @INC = @INC;
+    pop @INC if $INC[-1] eq '.';
     try {; require POE::Component::SSLify; $_can_haz{ssl} = 1 }
       catch {; $_can_haz{ssl} = 0 };
   }
@@ -18,6 +20,8 @@ sub has_ssl_support {
 
 sub has_zlib_support {
   unless (defined $_can_haz{zlib}) {
+    local @INC = @INC;
+    pop @INC if $INC[-1] eq '.';
     try {; require POE::Filter::Zlib::Stream; $_can_haz{zlib} = 1 }
       catch {; $_can_haz{zlib} = 0 };
   }
